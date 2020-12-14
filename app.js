ae8dbc31a8c4e790e10b3994ff280857d7463a84
mrkefir4ik <joker19980621@gmail.com>
2020-12-14 22:08:49 +0300

    feat : CORS problems

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const search = document.querySelector('.search');
 const button = document.querySelector('.button');
-let i = 0;
 
-button.onclick = function(){
-  alert(i++);
+function searcher () {
+  let x = search.value;
+  console.log(x);
+  $.ajax({
+    url: `https://superheroapi.com/api/2777162452537755/search/batman`,
+    dataType: "json",
+    
+    success: function(data){
+      console.log(data)
+    },
+
+    type: 'GET',
+
+    
+  })
 }
+
+button.onclick = searcher;
