6645ae3a44eb2d6d02f33b3df810388d9f171f34
mrkefir4ik <joker19980621@gmail.com>
2020-12-20 15:53:33 +0300

    refactor : total change of the idea. Now its LOTR wiki, because google books api doesnt work in Belarus

--- app.js
+++ app.js
@@ -2,20 +2,29 @@ const search = document.querySelector('.search');
 const button = document.querySelector('.button');
 
 function searcher () {
+
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://the-one-api.dev/v2/book",
+    "method": "GET",
+    "headers": {
+      "Authorization": "5eOAh3-2Ozp0GFsspTCC"
+    }
+  }
+
+
+
+
+
   let x = search.value;
   console.log(x);
-  $.ajax({
-    url: `https://superheroapi.com/api/2777162452537755/search/batman`,
-    dataType: "json",
-    
-    success: function(data){
-      console.log(data)
-    },
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
 
-    type: 'GET',
 
-    
-  })
 }
 
 button.onclick = searcher;
